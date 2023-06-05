4f5ecc2731af8f05e56ce8c2b0605aef40c3edfc
Alexxis36 <fedor4encko.dmitr@yandex.ru>
2023-06-05 12:02:26 +0300

    first

---
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string s = "Это 1строка23";
+    int col_chisla = 0;
+    for (int i = 0; i < size(s); i++)
+    {
+        if (s[i] >= '0' && s[i] <= '9') {
+            col_chisla++;
+        }
+    }
+    cout << "Количество чисел в строке = " << col_chisla;
+}
